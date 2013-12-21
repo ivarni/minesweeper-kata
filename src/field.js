24017c07b20fa38b92ead46df5f3856a783f8d23
Ivar Nilsen <post@ivarnilsen.com>
2013-12-21 13:31:48 +0100

    Counting mines

--- src/field.js
+++ src/field.js
@@ -1,6 +1,35 @@
-var Minefield = function(rows) {
-    this.rows = rows;
+var Minefield = function(field) {
+    var rows = this.rows = [];
+    field.forEach(function(row) {
+        rows.push(row.split(''));
+    });
+};
+Minefield.prototype.numberOfMines = function(i, j) {
+    return this.rows[i] && this.rows[i][j] === '*' ? 1 : 0;
+};
+Minefield.prototype.getAdjacentCount = function(i, j) {
+    return this.numberOfMines(i, j - 1) +
+        this.numberOfMines(i, j + 1) +
+        this.numberOfMines(i + 1, j - 1) +
+        this.numberOfMines(i + 1, j) +
+        this.numberOfMines(i + 1, j + 1) +
+        this.numberOfMines(i - 1, j - 1) +
+        this.numberOfMines(i - 1, j) +
+        this.numberOfMines(i - 1, j + 1);
 };
 Minefield.prototype.getRows = function() {
-    return this.rows;
+    var self = this;
+    var result = [];
+    this.rows.forEach(function(row, i) {
+        var calculated = [];
+        row.forEach(function(cell, j) {
+            if (cell === '*') {
+                calculated.push(cell);
+            } else {
+                calculated.push(self.getAdjacentCount(i, j));
+            }
+        });
+        result.push(calculated);
+    });
+    return result;
 };
--- test/unit/test.spec.js
+++ test/unit/test.spec.js
@@ -1,9 +1,39 @@
 describe('Calculating the field', function() {
 
     it('returns an empty row with no mines', function() {
-        var field = new Minefield(["..."]);
+        var field = new Minefield(['...']);
         var row = field.getRows()[0];
-        expect(row).toBe("...");
+        expect(row).toEqual([0, 0, 0]);
+    });
+
+    it('returns a row with one mine', function() {
+        var field = new Minefield(['*']);
+        var row = field.getRows()[0];
+        expect(row).toEqual(['*']);
+    });
+
+    it('counts one adjacent mine', function() {
+        var field = new Minefield(['*..']);
+        var row = field.getRows()[0];
+        expect(row).toEqual(['*', 1, 0]);
+    });
+
+    it('counts two adjacent mines', function() {
+        var field = new Minefield(['*.*']);
+        var row = field.getRows()[0];
+        expect(row).toEqual(['*', 2, '*']);
+    });
+
+    it('counts vertical mines', function() {
+        var field = new Minefield(['*', '.']);
+        var row = field.getRows()[1];
+        expect(row).toEqual([1]);
+    });
+
+    it('counts surrounding mines', function() {
+        var field = new Minefield(['***', '*.*', '***']);
+        var row = field.getRows()[1];
+        expect(row).toEqual(['*', 8, '*']);
     });
 
 });
